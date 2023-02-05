95b9d5ac9bdc39500df20387fd2cb1494ffdad72
Eric Gagnon <ergagnon@gmail.com>
2023-02-04 20:11:10 -0500

    Added base keys and interface

---
+++ cmder.go
@@ -0,0 +1,26 @@
+// Copyright © 2023 Eric Gagnon <github.com/ergagnon>
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+package gocmder
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type Cmder interface {
+	Run() error
+	CobraCmd() *cobra.Command
+	ViperCfg() *viper.Viper
+}
