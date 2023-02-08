5d4ed75496fbfc67f5d3a6ec39dfcefea8557977
Eric Gagnon <ergagnon@gmail.com>
2023-02-08 09:30:06 -0500

    Implement Cmder logic

--- cmder.go
+++ cmder.go
@@ -15,6 +15,10 @@
 package gocmder
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -52,6 +56,8 @@ func NewCmder(cfg any, onFinalize OnFinalizeFunc, opts ...CmderOption) *Cmder {
 		onFinalize(c.cfg)
 	})
 
+	c.init(createConfigItems(cfg))
+
 	return c
 }
 
@@ -67,8 +73,104 @@ func (c *Cmder) Execute() error {
 	return c.cobra.Execute()
 }
 
+func (c *Cmder) init(items []configItem) error {
+	
+	for _, item := range items {
+		if err := c.addCliFlag(item); err != nil {
+			return err
+		}
+		
+		if err := c.setDefaultConfigValue(item); err != nil {
+			return err
+		}
+
+		if err := c.connectViperAndCobra(item); err != nil {
+			return err
+		}
+	}
+	
+	return nil
+}
+
+func (c *Cmder) addCliFlag(item configItem) error {
+	if item.isHidden {
+		return nil
+	}
+	
+	switch item.kind {
+	case reflect.String:
+		c.cobra.Flags().String(item.name, item.defaultValue.(string), item.desc)
+	case reflect.Bool:
+		c.cobra.Flags().Bool(item.name, item.defaultValue.(bool), item.desc)
+	case reflect.Int:
+		c.cobra.Flags().Int(item.name, item.defaultValue.(int), item.desc)
+	default:
+		return fmt.Errorf("unsupported type %s", item.kind)
+	}
+	
+	if item.isRequired {
+		c.cobra.MarkFlagRequired(item.name)
+	}
+	
+	return nil
+}
+
+func (c *Cmder) setDefaultConfigValue(item configItem) error {
+	switch item.kind {
+	case reflect.String:
+		c.viper.SetDefault(item.name, item.defaultValue.(string))
+	case reflect.Bool:
+		c.viper.SetDefault(item.name, item.defaultValue.(bool))
+	case reflect.Int:
+		c.viper.SetDefault(item.name, item.defaultValue.(int))
+	default:
+		return fmt.Errorf("unsupported type %s", item.kind)
+	}
+
+	return nil
+}
+
+func (c *Cmder) connectViperAndCobra(item configItem) error {
+	if item.isHidden {
+		return nil
+	}
+	
+	if err := c.viper.BindPFlag(item.name, c.cobra.Flags().Lookup(toFlagName(item.name))); err != nil {
+		return err
+	}
+
+	if err := c.viper.BindEnv(item.name, toEnvName(c.envPrefix, item.name)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func toFlagName(name string) string {
+	return strings.ToLower(strings.Replace(name, ".", "-", -1))
+}
+
+func toEnvName(prefix, name string) string {
+	if prefix != "" {
+		name = prefix + "_" + name
+	} 
+
+	return strings.ToUpper(strings.Replace(name, ".", "_", -1))
+}
+
 func (c *Cmder) run() func(cmd *cobra.Command, _ []string) error {
 	return func(cmd *cobra.Command, _ []string) error {
-		return cmd.Help()
+
+		if err := c.viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+				return err
+			}
+		}
+
+		if err := c.viper.Unmarshal(c.cfg); err != nil {
+			return err
+		}
+
+		return nil 
 	}
 }
